e88f04414809b2151c42b181be362402efc17505
VibKkk <240785@edu.p.lodz.pl>
2022-01-27 16:06:01 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/git_project_01.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/git_project_01.dir/main.cpp.obj"
+  "git_project_01.exe"
+  "git_project_01.exe.manifest"
+  "git_project_01.pdb"
+  "libgit_project_01.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/git_project_01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/git_project_01.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for git_project_01.
+# This may be replaced when dependencies are built.
